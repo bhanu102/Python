1df93cd057b206870e496f77a60de48d47abb6d9
bhanu102 <bhanugadupudi45@gmail.com>
2021-12-13 13:46:49 +0530

    lambda & functional arguments

---
+++ functional_programming.py
@@ -0,0 +1,22 @@
+# # filter function
+# def func(x):
+#     if (x % 2 == 0 or x % 4 == 0):
+#         return x
+# result = list(filter(func, range(2,22)))
+# print(result)
+
+# # map function
+# def func(x):
+#     return x*x
+
+# num_list = [1,2,3,4,5,6]
+# result = list(map(func, num_list))
+# print(result)
+
+# # reduce function
+# import functools
+# def sum(x, y):
+#     return x+y
+
+# num_list = [1,2,3,4,5]
+# print(functools.reduce(sum, num_list))
---
+++ lambda.py
@@ -0,0 +1,13 @@
+# # sum of two numbers using lambda fn assigned to a variable
+# sum = lambda x,y: x+y
+# print(sum(1,2))
+
+# # without assigning to a variable
+# print((lambda x,y: x+y)(1,2))
+
+# # passing lambda as an argument to a function
+# def func(f,n):
+#     print(f(n))
+
+# f = lambda x: x+1
+# func(f, 1)
