71dc96f1bec5fabaa6b2dea99d41c33b932e24d1
bhanu102 <bhanugadupudi45@gmail.com>
2021-12-13 15:52:46 +0530

    comprehensions

---
+++ comprehensions.py
@@ -0,0 +1,15 @@
+# # list comprehension
+# my_list = [1,2,3,4,5,6]
+# result = [n*n for n in my_list]
+# print(result)
+
+# # dictionary comprehension
+# sub = ['maths','physics','chemistry','social']
+# marks = [79,77,76,75]
+# result = {sub:marks for sub,marks in zip(sub,marks)}
+# print(result)
+
+# # set comprehension
+# my_list = [1,1,1,2,2,2,3,2,2,3,4,5,6,6,6,7,3,7]
+# result = {n for n in my_list}
+# print(list(result))
